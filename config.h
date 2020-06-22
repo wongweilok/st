34fddcd5a71bc1b8fb377742e5bef4196da6c47e
Wong Wei Lok <dev@wongweilok.com>
2020-06-22 15:22:31 +0800

    Add boxdraw patch to improve lines and blocks characters rendering

--- config.h
+++ config.h
@@ -5,10 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 
 /* Fallback fonts */
-static char *font2[] = {"JoyPixels:pixelsize=14:antialias=true:autohint=true"};
+static char *font2[] = {
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true",
+	"Inconsolata Nerd Font:pixelsize=16:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
@@ -69,6 +72,18 @@ static unsigned int blinktimeout = 800;
  * thickness of underline and bar cursors
  */
 static unsigned int cursorthickness = 2;
+
+/*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 0;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
